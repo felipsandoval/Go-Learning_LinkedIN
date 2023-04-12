e85be2212c225d0515fef5904a6b92c29c115a07
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-12 09:04:07 +0000

    Starting course

--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,5 +1,13 @@
 // packages/basics/main.go
 package main
 
+// importing the fmt package from the pck go website
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	fmt.Println("Hello World!")
+	fmt.Printf("Today is %s", time.Now())
 }
