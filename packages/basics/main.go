f14df0d78ddbb19e8d3b3b720d48d06c87ffac42
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-12 11:06:07 +0000

    Advance and pre Challenge 1

--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,13 +1,50 @@
 // packages/basics/main.go
 package main
 
-// importing the fmt package from the pck go website
 import (
+	"errors"
 	"fmt"
-	"time"
+	"strconv"
 )
 
-func main() {
+// importing the fmt package from the pck go website
+/*func main() {
 	fmt.Println("Hello World!")
-	fmt.Printf("Today is %s", time.Now())
+	fmt.Printf("Today is %s", time.Now().Weekday())
+	fmt.Println("")
+}*/
+
+// A simple greet function
+func greet() string {
+	return "Hi!"
+}
+
+// A simple greet function with the name of the person
+func greetWithName(name string) string {
+	return "Hi, " + name + "!"
+}
+
+// A simple greet function with the name of the person
+func greetWithNameAndAge(name string, age int) (greeting string) {
+	greeting = "Hello, my name is " + name + " and I am " + strconv.Itoa(age) +
+		" years old."
+	return
+}
+
+// Function that divides by zero two input numbers. If is divided by zero raise
+// and exception
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil // nil is used to catch execution errors
+	// nil es el valor cero para punteros, interfaces, mapas, slices, canales y funciones; y corresponde a la representación de un valor no inicializado
+}
+
+func main() {
+	//fmt.Println(greet())
+	//fmt.Println(greetWithName("Claudia"))
+	//fmt.Println(greetWithNameAndAge("David", 29))
+	fmt.Println(divide(10, 2))
+	fmt.Println(divide(10, 0))
 }
