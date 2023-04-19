dc5d04cbdb75cca79dc2b88cb0960239268f10d2
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-19 15:44:05 +0000

    Finished flow-control exercises

--- flow-control/defer-panic-recover/begin/main.go
+++ flow-control/defer-panic-recover/begin/main.go
@@ -9,8 +9,21 @@ func cleanup(msg string) {
 
 func main() {
 	// defer function calls
+	// defer changes the flow-control within my program
+	defer cleanup("first")
+	defer cleanup("second")
 
+	fmt.Println("Starting my script")
 	// defer recovery
+	defer func(){
+		if err := recover(); err != nil{
+			fmt.Println("recover from panic:", err)
+		}
+	}()
 
 	// panic
+	panic("something bad happened")
 }
+
+// This might felt similar to a try-catch in other languages but go encourages to use and handle errors as values
+// 
