0ab3aec2c681c0acaee6facdb0db755179ee6e2d
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-20 09:19:43 +0000

    Advance in challenge 4

--- challenges/flow-control/begin/main.go
+++ challenges/flow-control/begin/main.go
@@ -1,10 +1,29 @@
 // challenges/flow-control/begin/main.go
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+type count map[string]string
+
+func handleFiles() {
+	//data, err := os.ReadFile("challenges/data.txt")
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		panic(fmt.Errorf("failed to read the file: %s", err))
+	}
+	os.Stdout.Write(data)
+	fmt.Println() // to print a new line for not having all the information within the same line tho 
+}
+
 func main() {
 	// handle any panics that might occur anywhere in this function
 	//
-
+	fmt.Println("Starting the challenge #4")
+	//os.Stderr.Read([]byte, os.Args[])
+	handleFiles()
 	// use os package to read the file specified as a command line argument
 	//
 
@@ -13,7 +32,9 @@ func main() {
 
 	// initialize a map to store the counts
 	//
-
+	/*counter := count {}
+	counter := count{"aloha": "aloha"}
+	fmt.Printf("\nThis is my map: %#v\n", counter)*/
 	// use the standard library utility package that can help us split the string into words
 	//
 
