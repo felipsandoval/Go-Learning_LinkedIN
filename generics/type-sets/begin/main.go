6a19783b70368462c6791d4a61cc19d7e739d781
Felipe Enmanuel Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-25 11:59:51 +0200

    Module 5 without challenge

--- generics/type-sets/begin/main.go
+++ generics/type-sets/begin/main.go
@@ -4,13 +4,25 @@ package main
 import "fmt"
 
 // create a numeric interface with a type set
+type numeric interface {
+	~int | ~float64
+	grow()
+}
+
+// The benefit from doing it this way is that numeric is an interface like any other like
+// we are accustomed to and go. We can add a method to it and require that any type numeric
+// also have that method
 
 // update sum function to use a numeric interface with a type set
-func sum[T ~int | ~float64](a, b T) T {
+func sum[T numeric](a, b T) T {
 	return a + b
 }
 
 type specialInt int
+
+func (si specialInt) grow() {
+
+}
 
 func main() {
 	one := specialInt(1)
