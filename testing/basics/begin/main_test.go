cb0d1dc8df8669fb5d05e8364207d9fb333fcbe6
Felipe Enmanuel Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-25 13:14:54 +0200

    Text cases. Module 6

--- testing/basics/begin/main_test.go
+++ testing/basics/begin/main_test.go
@@ -1,6 +1,25 @@
 // testing/basics/begin/main_test.go
 package main
 
+import (
+	"fmt"
+	"testing"
+)
+
+// Tp execute it it needs a go test and name of the file
 // write a test for sum
+func TestSum(t *testing.T) {
+	got := sum(1, 2, 3)
+	want := 6
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("***SETUP***")
+	m.Run()
+	fmt.Println("***TEARDOWN***")
+}
 
 // write a TestMain for setup and teardown
